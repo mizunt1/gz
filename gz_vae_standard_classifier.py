b5d9c2aaa3c348bf7ca7c356a46075e58509c166
mizunt1 <mizunt@gmail.com>
2020-06-03 12:20:42 +0100

    uses test data to test

--- gz_vae_standard_classifier.py
+++ gz_vae_standard_classifier.py
@@ -7,11 +7,12 @@ from pyro.infer import SVI, Trace_ELBO
 a01 = "t01_smooth_or_features_a01_smooth_count"
 a02 = "t01_smooth_or_features_a02_features_or_disk_count"
 a03 = "t01_smooth_or_features_a03_star_or_artifact_count"
-data = Gz2_data(csv_file="gz2_4.csv",
-                root_dir="~/diss/gz/gz2_mini/",
+data = Gz2_data(csv_file="gz_amended.csv",
+                root_dir="~/diss/gz2_data/",
                 list_of_interest=[a01,
                                   a02,
                                   a03])
+
 
 vae = VAE()
 
@@ -22,16 +23,23 @@ train_elbo = []
 test_elbo = []
 USE_CUDA = False
 TEST_FREQUENCY = 1
-train_loader = torch.utils.data.DataLoader(dataset=data, batch_size=2)
-test_loader = torch.utils.data.DataLoader(dataset=data, batch_size=2)
-# training loop
-for epoch in range(3):
+
+train_indices = list(i for i in range(0,800))
+test_indices = list(i for i in range(800, 1000))
+train_set = torch.utils.data.Subset(data, train_indices)
+test_set =  torch.utils.data.Subset(data, test_indices)
+train_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=20)
+test_loader = torch.utils.data.DataLoader(dataset=test_set, batch_size=20)
+
+
+# training VAE
+
+for epoch in range(10):
     print("training")
     total_epoch_loss_train = train(svi, train_loader, use_cuda=USE_CUDA)
     print("end train")
     train_elbo.append(-total_epoch_loss_train)
     print("[epoch %03d]  average training loss: %.4f" % (epoch, total_epoch_loss_train))
-
     if epoch % TEST_FREQUENCY == 0:
         # report test diagnostics
         print("evaluating")
@@ -39,3 +47,5 @@ for epoch in range(3):
         test_elbo.append(-total_epoch_loss_test)
         print("[epoch %03d] average test loss: %.4f" % (epoch, total_epoch_loss_test))
         print("evaluate end")
+        #torch.save(vae.encoder.state_dict(), "encoder.checkpoint")
+        #torch.save(vae.decoder.state_dict(), "decoder.checkpoint")
