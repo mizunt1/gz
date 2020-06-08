0c195d6d718083fafdd8410d376d593168f69890
mizunt1 <mizunt@gmail.com>
2020-06-08 09:04:40 +0100

    1 channel instead of rgb

--- load_gz_data.py
+++ load_gz_data.py
@@ -1,41 +1,43 @@
 import pandas as pd
 import torch
 import os
+from PIL import Image
 from skimage import io
+import torchvision as tv
 
 class Gz2_data(torch.utils.data.Dataset):
-    def __init__(self, csv_file, root_dir, list_of_interest):
-        self.csv_file = csv_file
-        self.root_dir = root_dir
-        self.file_path = os.path.join(self.root_dir, self.csv_file)
-        self.file = pd.read_csv(self.file_path)
-        self.root_dir = root_dir
+    def __init__(self, csv_dir, image_dir, list_of_interest, faulty_data_set=False):
+        self.csv_dir = csv_dir
+        self.faulty_data_set = faulty_data_set
+        self.image_dir = image_dir
+        self.file = pd.read_csv(self.csv_dir)
         self.list_of_interest = list_of_interest
 
     def __len__(self):
         return len(self.file)
 
-    def __getitem__(self, idx, falty_data_set=False):
+    def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        if falty_data_set == True:
+        if self.faulty_data_set == True:
             image = None
             while image is None:
                 try:
-                    img_name = os.path.join(self.root_dir, self.file['png_loc'][idx])
+                    img_name = os.path.join(self.image_dir, self.file['png_loc'][idx])
                     img_name = img_name.replace(".png", ".jpg")
-                    image = io.imread(img_name)
+                    image = Image.open(img_name).convert('L')
                 except FileNotFoundError:
                     idx += 1
         else:
-            img_name = os.path.join(self.root_dir, self.file['png_loc'][idx])
+            img_name = os.path.join(self.image_dir, self.file['png_loc'][idx])
             img_name = img_name.replace(".png", ".jpg")
-            image = io.imread(img_name)
+            image = Image.open(img_name).convert('L')
+
         data = self.file.iloc[idx][self.list_of_interest]
         data = torch.tensor(data.values.astype('int32'))
-        image = torch.tensor(image)
-        # permute to NCHW
-        image = image.permute(2,0,1).float()
+        trans = tv.transforms.ToTensor()
+        image = trans(image)
+        print(image.shape)
         sample = {'image': image, 'data': data}
 
         return sample
@@ -44,11 +46,13 @@ if __name__ == "__main__":
     a01 = "t01_smooth_or_features_a01_smooth_count"
     a02 = "t01_smooth_or_features_a02_features_or_disk_count"
     a03 = "t01_smooth_or_features_a03_star_or_artifact_count"
-    data = Gz2_data(csv_file="gz2_20.csv",
-                    root_dir="~/diss/gz2_data",
+    # not sure why PIL doesnt work with ~
+    data = Gz2_data(csv_dir="~/diss/gz2_data/gz_amended.csv",
+                    image_dir="/Users/Mizunt/diss/gz2_data",
                     list_of_interest=[a01,
                                       a02,
-                    a03])
+                                      a03], faulty_data_set=False)
     sample_of_data = data[1]
     print(type(sample_of_data['data']))
+    print(sample_of_data['image'].shape)
     print(type(sample_of_data['image']))
