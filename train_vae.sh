cf8a7a397c54d4abf84f25d0e462a0c3f9bc4ee2
mizunt1 <mizunt@gmail.com>
2020-06-08 11:24:35 +0100

    added bash script for slurm

---
+++ train_vae.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+
+#SBATCH --output=slurm-%j.out
+#SBATCH --error=slurm-%j.err
+#SBATCH --job-name="big-bayesian-model"
+
+export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
+export CONDA_PKGS_DIRS=/scratch-ssd/$USER/conda_pkgs
+
+/scratch-ssd/oatml/scripts/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f environment.yml
+source /scratch-ssd/oatml/miniconda3/bin/activate my_env.yml
+
+srun python gz_vae_train.py\
+     --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
+     --img_file /scratch-ssd/oatml/data/gz2\
+     --use_cuda True
