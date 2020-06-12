bd5daf35af377fca4a412c0df8be7662b6b6a011
mizunt1 <mizunt@gmail.com>
2020-06-12 15:20:31 +0100

    can now load and train fashion mnist

---
+++ train_ssvae.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+
+#SBATCH --output=slurm-%j.out
+#SBATCH --error=slurm-%j.err
+#SBATCH --job-name="mnist_ssvae"
+#SBATCH --partition="msc"
+
+export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
+export CONDA_PKGS_DIRS=/scratch-ssd/$USER/conda_pkgs
+
+/scratch-ssd/oatml/scripts/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f gz_mizu.yml
+echo 'env created'
+source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
+echo 'env activated'
+srun python train_ssvae.py
+echo 'script ran'
