6d7e14a1b29a3993b0763e15d236390661ce4079
mizunt1 <mizunt@gmail.com>
2020-06-17 16:31:28 +0100

    updated bash for ssvae

--- train_ssvae_fash.sh
+++ train_ssvae_fash.sh
@@ -4,7 +4,7 @@
 
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
-#SBATCH --job-name="fashion"
+#SBATCH --job-name="mnist_ssvae"
 #SBATCH --partition="msc"
 
 export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
@@ -14,6 +14,5 @@ export CONDA_PKGS_DIRS=/scratch-ssd/$USER/conda_pkgs
 echo 'env created'
 source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
 echo 'env activated'
-srun python train_ssvae.py \
-     --data_save tb_data_fash --data_type "fashion" --epochs 200
+srun python train_ssvae.py --data_type fashion --data_save tb_data
 echo 'script ran'
