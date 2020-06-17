d46cf3e3912679d16f048efd33d66a2c09292d5c
mizunt1 <mizunt@gmail.com>
2020-06-17 16:59:35 +0100

    chck point cmd line args

--- gz_vae_train.py
+++ gz_vae_train.py
@@ -2,6 +2,7 @@ from gz_vae_conv import VAE, train, evaluate
 from torch.utils.tensorboard import SummaryWriter
 from load_gz_data import Gz2_data, return_data_loader
 from simple_classifier import Classifier
+import torchvision
 import torch
 from pyro.optim import Adam
 from pyro.infer import SVI, Trace_ELBO
@@ -11,15 +12,17 @@ parser = argparse.ArgumentParser()
 csv = "gz2_mini/gz2_4.csv"
 img = "gz2_mini/"
 parser.add_argument('--writer', required=True)
+parser.add_argument('--checkpoint_dir', required=True)
 parser.add_argument('--csv_file', metavar='c', type=str, default=csv)
 parser.add_argument('--img_file', metavar='i', type=str, default=img)
 parser.add_argument('--no_cuda', default=False, action='store_true')
 parser.add_argument('--num_epochs', type=int, default=3)
 
+
+
+args = parser.parse_args()
 writer = SummaryWriter("tb_data_all/" + args.writer)
 use_cuda = not args.no_cuda
-args = parser.parse_args()
-
 a01 = "t01_smooth_or_features_a01_smooth_count"
 a02 = "t01_smooth_or_features_a02_features_or_disk_count"
 a03 = "t01_smooth_or_features_a03_star_or_artifact_count"
@@ -36,13 +39,14 @@ optimizer = Adam({"lr": 1.0e-3})
 svi = SVI(vae.model, vae.guide, optimizer, loss=Trace_ELBO())
 
 
-batch_size = 1
+batch_size = 2
 test_proportion = 0.5
 train_loader, test_loader = return_data_loader(data, test_proportion, batch_size)
 
 test_freq = 1
 # training VAE
 plot_img_freq = 1
+checkpoint_freq = 1
 
 for epoch in range(args.num_epochs):
     print("training")
@@ -57,11 +61,18 @@ for epoch in range(args.num_epochs):
         print("evaluate end")
         writer.add_scalar('Train loss', total_epoch_loss_train, epoch)
         writer.add_scalar('Test loss', total_epoch_loss_test, epoch)
-
+        print(epoch)
     if epoch % plot_img_freq == 0:
-        one_image = next(iter(train_loader))['image'][0]
+        one_image = next(iter(train_loader))['image'][0:1]
         print("one_image", one_image.shape)
         images_out = vae.sample_img(one_image, use_cuda=use_cuda)
         img_grid = torchvision.utils.make_grid(images_out)
-        writer.add_image('images from epoch'+ int(epoch), img_grid)
+        writer.add_image('images from epoch'+ str(epoch), img_grid)
+
+    if epoch % checkpoint_freq == 0:
+        torch.save(ssvae.encoder_y.state_dict(), "checkpoints/" + args.checkpoint_dir + "/encoder_y.checkpoint")
+        torch.save(ssvae.encoder_z.state_dict(), "checkpoints/" + args.checkpoint_dir + "/encoder_z.checkpoint")
+        torch.save(ssvae.decoder.state_dict(),  "checkpoints/" + args.checkpoint_dir +  "/decoder.checkpoint")
+    
+
     writer.close()
