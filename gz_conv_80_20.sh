afadcc981ba3c5d81af8fd88ab6b9c7b54a9c145
mizunt1 <mizunt@gmail.com>
2020-06-22 09:36:20 +0100

    trainer

--- gz_conv_80_20.sh
+++ gz_conv_80_20.sh
@@ -4,7 +4,7 @@
 
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
-#SBATCH --job-name="32gz_long"
+#SBATCH --job-name="gz_elu"
 #SBATCH --partition="msc"
 
 export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
@@ -13,9 +13,9 @@ export CONDA_PKGS_DIRS=/scratch-ssd/$USER/conda_pkgs
 /scratch-ssd/oatml/scripts/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f gz_mizu.yml
 source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
 
-srun python train_small_conv.py\
+srun python trainer_vae.py\
     --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
     --img_file /scratch-ssd/oatml/data/gz2\
-    --dir_name gz_conv_32_long\
-    --num_epochs 100 --img_size 80 --crop_size 80 --z_size 40
+    --dir_name gz_conv_elu\
+    --num_epochs 70 --img_size 80 --crop_size 80 --z_size 40
 
