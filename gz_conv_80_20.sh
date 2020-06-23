90fc35d6a980cf7714fec695ffae415082629eb5
mizunt1 <mizunt@gmail.com>
2020-06-23 20:04:14 +0100

    batch norm training

--- gz_conv_80_20.sh
+++ gz_conv_80_20.sh
@@ -4,7 +4,7 @@
 
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
-#SBATCH --job-name="gz_r2"
+#SBATCH --job-name="res_batchn"
 #SBATCH --partition="msc"
 
 export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
@@ -16,6 +16,7 @@ source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
 srun python trainer_vae.py\
     --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
     --img_file /scratch-ssd/oatml/data/gz2\
-    --dir_name gz_round2.1\
-    --num_epochs 70 --img_size 80 --crop_size 80 --z_size 40
+    --arch encoder_decoder_res_batchn.py\
+    --dir_name res_batchn\
+    --num_epochs 500 --img_size 80 --crop_size 80 --z_size 40
 
