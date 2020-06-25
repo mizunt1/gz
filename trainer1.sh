6c2434fb0c651c567aeb8334df88e7921b4c79fc
mizunt1 <mizunt@gmail.com>
2020-06-25 17:17:05 +0100

    img size 104 lr3

--- trainer1.sh
+++ trainer1.sh
@@ -16,6 +16,6 @@ source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
 srun python trainer_vae.py\
     --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
     --img_file /scratch-ssd/oatml/data/gz2\
-    --dir_name img_size_128_lr4\
+    --dir_name img_size_104_lr3\
     --arch encoder_decoder_larger_kern.py --lr 1.0e-4\
-    --num_epochs 700 --img_size 128 --crop_size 128 --z_size 130 --subset
+    --num_epochs 700 --img_size 104 --crop_size 104 --z_size 130 --subset
