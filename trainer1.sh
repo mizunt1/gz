9f7e4cd617b18190707bd804a3f1525ba772f9a6
mizunt1 <mizunt@gmail.com>
2020-06-26 11:05:31 +0100

    training with larger kern sizes

--- trainer1.sh
+++ trainer1.sh
@@ -16,6 +16,6 @@ source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
 srun python trainer_vae.py\
     --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
     --img_file /scratch-ssd/oatml/data/gz2\
-    --dir_name encoder_decoder_large_k_size\ 
-    --arch encoder_decoder_large_k_size.py   --lr 1.0e-3\
+    --dir_name encoder_decoder_large_k_size\
+    --arch encoder_decoder_large_k_size.py  --lr 1.0e-3\
     --num_epochs 1400 --img_size 80 --crop_size 80 --z_size 100 --subset
