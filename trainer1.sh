c5d201120684800cd4f0b57fde8acda06bd0e854
mizunt1 <mizunt@gmail.com>
2020-06-26 17:05:03 +0100

    training whole data but with less cropping

--- trainer1.sh
+++ trainer1.sh
@@ -4,7 +4,7 @@
 
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
-#SBATCH --job-name="whole data"
+#SBATCH --job-name="whole104"
 #SBATCH --partition="msc"
 
 export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
@@ -16,6 +16,6 @@ source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
 srun python trainer_vae.py\
     --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
     --img_file /scratch-ssd/oatml/data/gz2\
-    --dir_name encoder_decoder_new_res_whole\
-    --arch encoder_decoder_new_res3.py  --lr 1.0e-3\
-    --num_epochs 2400 --img_size 80 --crop_size 80 --z_size 100 --batch_size 100
+    --dir_name encoder_decoder_new_res_whole_104\
+    --arch encoder_decoder_new_res3.py  --lr 1.0e-2\
+    --num_epochs 3400 --img_size 104 --crop_size 104 --z_size 100 --batch_size 100
