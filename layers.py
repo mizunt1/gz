091e7eb2b04e630da4833b647d5cff008766e91a
mizunt1 <mizunt@gmail.com>
2020-06-26 11:37:24 +0100

    added lewis' layers

--- layers.py
+++ layers.py
@@ -36,3 +36,43 @@ class LinearBlock(nn.Module):
 class Flatten(nn.Module):
     def forward(self, input):
         return input.view(input.size(0), -1)
+
+
+class UpResBloc(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.body = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1),
+            nn.ELU(),
+            nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1),
+            nn.ELU(),
+            nn.Upsample(scale_factor=2, mode="nearest"),
+        )
+        self.skip = nn.Upsample(scale_factor=2, mode="nearest")
+        self.merge = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        return self.merge(self.skip(x) + self.body(x))
+
+
+class DownResBloc(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.body = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1, bias=False),
+            nn.ELU(),
+            nn.BatchNorm2d(in_channels),
+            nn.Conv2d(in_channels, in_channels, kernel_size=3, padding=1, bias=False),
+            nn.ELU(),
+            nn.BatchNorm2d(in_channels),
+            nn.MaxPool2d(2),
+        )
+        self.skip = nn.MaxPool2d(scale_factor=2, mode="nearest")
+        self.merge = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        return self.merge(self.skip(x) + self.body(x))
+
+if __name__ == "__main__":
+    x = torch.zeros([10, 1, 80, 80])
+    
