25b9d2a83c35a379b48694beb77a8398bf12aa77
mizunt1 <mizunt@gmail.com>
2020-06-28 15:35:27 +0100

    training bar or no bar after fixing bug

--- trainer2.sh
+++ trainer2.sh
@@ -9,14 +9,14 @@
 
 export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
 export CONDA_PKGS_DIRS=/scratch-ssd/$USER/conda_pkgs
-
+rsync -az clpc144:/scratch-ssd/oatml/data/gz2/ /scratch-ssd/oatml/data/gz2
 /scratch-ssd/oatml/scripts/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f gz_mizu.yml
 source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
 
 srun python trainer_vae_classifier_bar.py\
     --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
     --img_file /scratch-ssd/oatml/data/gz2\
-    --dir_name bar_no_bar\
-    --arch encoder_decoder_larger_kern.py\
-    --num_epochs 700 --img_size 80 --crop_size 80 --z_size 100  --batch_size 100\
+    --dir_name bar_no_bar2\
+    --arch encoder_decoder_new_res3.py\
+    --num_epochs 2700 --img_size 80 --crop_size 80 --z_size 100  --batch_size 100\
     --load_checkpoint classifier_test_fin
