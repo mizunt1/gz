a0d0370ea297a761e05c115f4bb87199f738d732
mizunt1 <mizunt@gmail.com>
2020-06-28 14:39:59 +0100

    training bar no bar

--- trainer2.sh
+++ trainer2.sh
@@ -4,19 +4,19 @@
 
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
-#SBATCH --job-name="lowerlr"
+#SBATCH --job-name="bar_nobar"
 #SBATCH --partition="msc"
 
 export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
 export CONDA_PKGS_DIRS=/scratch-ssd/$USER/conda_pkgs
-rsync -az clpc144:/scratch-ssd/oatml/data/gz2/ /scratch-ssd/oatml/data/gz2
+
 /scratch-ssd/oatml/scripts/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f gz_mizu.yml
 source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
 
-srun python trainer_vae_classifier.py\
+srun python trainer_vae_classifier_bar.py\
     --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
     --img_file /scratch-ssd/oatml/data/gz2\
-    --dir_name sanity\
-    --arch encoder_decoder_new_res3.py  --lr 1.0e-3\
-    --num_epochs 2400 --img_size 80 --crop_size 80 --z_size 100 --batch_size 100\
+    --dir_name bar_no_bar\
+    --arch encoder_decoder_larger_kern.py\
+    --num_epochs 700 --img_size 80 --crop_size 80 --z_size 100  --batch_size 100\
     --load_checkpoint classifier_test_fin
