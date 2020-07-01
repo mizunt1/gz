9f27446bf7288d71a67ab9a67a57e45d889aacae
mizunt1 <mizunt@gmail.com>
2020-07-01 23:27:00 +0100

    training again due to logging error

--- trainer1.sh
+++ trainer1.sh
@@ -16,6 +16,6 @@ source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
 srun python trainer_ssvae_multinom.py\
     --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
     --img_file /scratch-ssd/oatml/data/gz2\
-    --dir_name ssvae_multinom2\
-    --arch ss_encoders_decoders_gz_enum.py  --lr 1.0e-4\
-    --num_epochs 3400 --img_size 80 --crop_size 80 --z_size 100 --batch_size 100
+    --dir_name ssvae_multinom4\
+    --arch ss_encoders_decoders_gz_enum.py  --lr 1.0e-5\
+    --num_epochs 3400 --img_size 80 --crop_size 80 --z_size 100 --batch_size 250
