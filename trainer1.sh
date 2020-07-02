0345d22f89711fdaf27b8432a2313c09bf92fc41
mizunt1 <mizunt@gmail.com>
2020-07-02 11:07:24 +0100

    training smooth or featured without one hot

--- trainer1.sh
+++ trainer1.sh
@@ -4,7 +4,7 @@
 
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
-#SBATCH --job-name="ss bar"
+#SBATCH --job-name="ss sf"
 #SBATCH --partition="msc"
 
 export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
@@ -16,6 +16,6 @@ source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
 srun python trainer_ssvae_multinom.py\
     --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
     --img_file /scratch-ssd/oatml/data/gz2\
-    --dir_name ssvae_multinom_bar2\
+    --dir_name ssvae_multinom_not_one_hot\
     --arch ss_encoders_decoders_gz_enum.py  --lr 1.0e-4\
-    --num_epochs 3400 --img_size 80 --crop_size 80 --z_size 100 --batch_size 100 --bar_no_bar --y_size 2
+    --num_epochs 3400 --img_size 80 --crop_size 80 --z_size 100 --batch_size 100 
