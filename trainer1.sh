6b6124c1afc15426273a4e4ef557a203650f4e33
mizunt1 <mizunt@gmail.com>
2020-07-02 12:22:35 +0100

    not training not sure why try again

--- trainer1.sh
+++ trainer1.sh
@@ -4,7 +4,7 @@
 
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
-#SBATCH --job-name="ss sf"
+#SBATCH --job-name="ss sf2"
 #SBATCH --partition="msc"
 
 export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
@@ -16,6 +16,6 @@ source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
 srun python trainer_ssvae_multinom.py\
     --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
     --img_file /scratch-ssd/oatml/data/gz2\
-    --dir_name ssvae_multinom_not_one_hot\
+    --dir_name ssvae_multinom_not_one_hot2\
     --arch ss_encoders_decoders_gz_enum.py  --lr 1.0e-4\
     --num_epochs 3400 --img_size 80 --crop_size 80 --z_size 100 --batch_size 100 
