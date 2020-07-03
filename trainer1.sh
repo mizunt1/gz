b85dc3701b937928a5bce02902f9577e552a3ae0
mizunt1 <mizunt@gmail.com>
2020-07-03 21:41:36 +0100

    training no decoder with half data

--- trainer1.sh
+++ trainer1.sh
@@ -16,6 +16,6 @@ source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
 srun python trainer_without_decoder.py\
     --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
     --img_file /scratch-ssd/oatml/data/gz2\
-    --dir_name trainer_without_decoder_full_data\
+    --dir_name trainer_without_decoder_half_data\
     --arch encoder_decoder_new_res3.py  --lr 1.0e-4\
-    --num_epochs 3400 --img_size 80 --crop_size 80 --z_size 100 --batch_size 100 
+    --num_epochs 3400 --img_size 80 --crop_size 80 --z_size 100 --batch_size 100 --subset 
