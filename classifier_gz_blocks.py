092a17da7140f7d287841c591ae227cee2adf282
mizunt1 <mizunt@gmail.com>
2020-07-08 14:45:56 +0100

    new classifier

---
+++ classifier_gz_blocks.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+from layers import LinearBlock
+
+class Classifier(nn.Module):
+    def __init__(self, in_dim=100, hidden=200, out_dim=3):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(in_dim, hidden),
+            LinearBlock(hidden, hidden),
+            LinearBlock(hidden, int(hidden /2)),
+            LinearBlock((hidden/2), out_dim),
+            nn.ELU()
+        )
+
+
+    def forward(self, x):
+        return self.net(x)
+
