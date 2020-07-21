15a946fc70b042e7e52642cc6aac039e1c2952fc
mizunt1 <mizunt@gmail.com>
2020-07-21 16:19:24 +0100

    training but with 128

--- trainer3.sh
+++ trainer3.sh
@@ -4,7 +4,7 @@
 
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
-#SBATCH --job-name="ed_mike"
+#SBATCH --job-name="ed_mike128"
 #SBATCH --partition="msc"
 
 export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
@@ -16,7 +16,7 @@ source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
 srun python trainer_without_decoder.py\
     --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
     --img_file /scratch-ssd/oatml/data/gz2\
-    --dir_name trainer_wod_mike\
+    --dir_name trainer_wod_mike_128\
     --arch encoder_decoder_mike.py  --lr 1.0e-4\
-    --num_epochs 200 --img_size 80 --crop_size 80 --z_size 100 --batch_size 100
+    --num_epochs 200 --img_size 128 --crop_size 128 --z_size 100 --batch_size 100
 
