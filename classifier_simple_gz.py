e0a273063a71961a6ca10876489d3f989799e873
mizunt1 <mizunt@gmail.com>
2020-07-23 10:56:53 +0100

    training exactly as model in paper

--- classifier_simple_gz.py
+++ classifier_simple_gz.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 
 class Classifier(nn.Module):
-    def __init__(self, in_dim=100, hidden=200, out_dim=3):
+    def __init__(self, in_dim=100, hidden=128, out_dim=3):
         super().__init__()
         self.fc1 = nn.Linear(in_dim, hidden)
         self.fc2 = nn.Linear(hidden, out_dim)
