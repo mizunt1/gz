6d86367fc7ced2186a1609d92ca67014eaf424a0
mizunt1 <mizunt@gmail.com>
2020-07-23 14:57:29 +0100

    training pose vae on whole data

--- trainer5.sh
+++ trainer5.sh
@@ -4,7 +4,7 @@
 
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
-#SBATCH --job-name="simple"
+#SBATCH --job-name="pose"
 #SBATCH --partition="msc"
 
 export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
@@ -13,10 +13,10 @@ export CONDA_PKGS_DIRS=/scratch-ssd/$USER/conda_pkgs
 /scratch-ssd/oatml/scripts/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f gz_mizu.yml
 source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
 
-srun python trainer_vae_classifier.py\
+srun python trainer_pose_vae.py\
     --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
     --img_file /scratch-ssd/oatml/data/gz2\
-    --dir_name encoder_mike_softmax_simple\
-    --arch encoder_decoder_mike.py  --lr 1.0e-4\
-    --num_epochs 500 --img_size 128 --crop_size 128 --z_size 100 --batch_size 100 --subset --subset_portion 0.001
+    --dir_name pose_vae_whole_data\
+    --arch encoder_decoder_pose_vae.py  --lr 1.0e-4\
+    --num_epochs 200 --img_size 128 --crop_size 128 --z_size 100 --batch_size 100
 
