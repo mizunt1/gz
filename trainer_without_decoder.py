43b10599dd747f56467af01de27cc0790695d74d
mizunt1 <mizunt@gmail.com>
2020-07-23 10:36:45 +0100

    training mikes witout bug

--- trainer_without_decoder.py
+++ trainer_without_decoder.py
@@ -98,10 +98,10 @@ def evaluate_vae_classifier(vae, vae_loss_fn, classifier, classifier_loss_fn, te
             y = y.cuda()
         # step of elbo for vae
         vae_loss = vae_loss_fn(vae.model, vae.guide, x)
-        z_loc, z_scale = vae.encoder(x)
-        combined_z = torch.cat((z_loc, z_scale), 1)
-        combined_z = combined_z.detach()
-        y_out = classifier.forward(combined_z)
+        z_loc, z_scale, split = vae.encoder(x)
+        # combined_z = torch.cat((z_loc, z_scale), 1)
+        # combined_z = combined_z.detach()
+        y_out = classifier.forward(split)
         classifier_loss = classifier_loss_fn(y_out, y)
         total_acc += torch.sum(torch.eq(y_out.argmax(dim=1),y.argmax(dim=1)))
         epoch_loss_vae += vae_loss.item()
@@ -134,9 +134,9 @@ def train_vae_classifier(vae, vae_optim, vae_loss_fn, classifier, classifier_opt
             y = y.cuda()
         # step of elbo for vae
         classifier_optim.zero_grad()
-        z_loc, z_scale = vae.encoder(x)
-        combined_z = torch.cat((z_loc, z_scale), 1)
-        y_out = classifier.forward(combined_z)
+        z_loc, z_scale, split = vae.encoder(x)
+        # combined_z = torch.cat((z_loc, z_scale), 1)
+        y_out = classifier.forward(split)
         classifier_loss = classifier_loss_fn(y_out, y)
         # step through classifier
         total_loss = classifier_loss
@@ -243,7 +243,7 @@ print("train and log")
 
 vae_optim = Adam(vae.parameters(), lr= args.lr, betas= (0.90, 0.999))
 
-classifier = Classifier(in_dim=args.z_size*2)
+classifier = Classifier(in_dim=vae.encoder.linear_size)
 
 params = list(classifier.parameters()) + list(vae.encoder.parameters())
 classifier_optim = Adam(params, args.lr , betas=(0.90, 0.999))
