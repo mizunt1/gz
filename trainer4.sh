c2f174cdcc4a556ad5a57b31fe45fd74b2472d43
mizunt1 <mizunt@gmail.com>
2020-07-27 16:40:13 +0100

    ready to train pose ss vae

--- trainer4.sh
+++ trainer4.sh
@@ -4,7 +4,7 @@
 
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
-#SBATCH --job-name="ss enum"
+#SBATCH --job-name="ss2"
 #SBATCH --partition="msc"
 
 export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
@@ -13,10 +13,11 @@ export CONDA_PKGS_DIRS=/scratch-ssd/$USER/conda_pkgs
 /scratch-ssd/oatml/scripts/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f gz_mizu.yml
 source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
 
-srun python train_mike_arch.py\
-    --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
-    --img_file /scratch-ssd/oatml/data/gz2\
-    --dir_name mike_paper_check_wholedata\
-    --num_epochs 200 --img_size 128 --crop_size 128   --batch_size 100 \
-    --lr 1e-4
+srun python trainer_vae_classifier_ss.py\
+     --arch encoder_decoder_pose_vae_split.py\
+     --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
+     --img_file /scratch-ssd/oatml/data/gz2\
+     --dir_name trainer_classifier_ss\
+     --num_epochs 200 --img_size 128 --crop_size 128   --batch_size 100 \
+     --lr 1e-4 --us_proportion 0.8 --subset
 
