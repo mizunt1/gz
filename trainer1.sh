94b76c7d38f109369691355a38e8d40de708e716
mizunt1 <mizunt@gmail.com>
2020-07-30 22:12:08 +0100

    mike arch without decoder tests subset 0.3

--- trainer1.sh
+++ trainer1.sh
@@ -4,7 +4,7 @@
 
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
-#SBATCH --job-name="poselr5"
+#SBATCH --job-name="mike_0.3"
 #SBATCH --partition="msc"
 
 export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
@@ -13,9 +13,10 @@ export CONDA_PKGS_DIRS=/scratch-ssd/$USER/conda_pkgs
 /scratch-ssd/oatml/scripts/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f gz_mizu.yml
 source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
 
-srun python trainer_pose_vae.py\
+srun python trainer_without_decoder.py\
     --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
     --img_file /scratch-ssd/oatml/data/gz2\
-    --dir_name pose_lr5\
-    --arch encoder_decoder_pose_vae.py  --lr 1.0e-5\
-    --num_epochs 200 --img_size 128 --crop_size 128 --z_size 100 --batch_size 100
+    --dir_name wod_mike_0.3\
+    --arch encoder_decoder_mike.py  --lr 1.0e-4\
+    --num_epochs 200 --img_size 128 --crop_size 128 --z_size 100 --batch_size 100\
+    --subset_proportion 0.3
