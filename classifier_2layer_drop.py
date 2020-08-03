b30b6f65c5a51f1daf557b2fe95d7d369c517bdb
mizunt1 <mizunt@gmail.com>
2020-08-03 15:35:17 +0100

    new classifier with drop out

---
+++ classifier_2layer_drop.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+
+class Classifier(nn.Module):
+    def __init__(self, in_dim=100, hidden=128, out_dim=3):
+        super().__init__()
+        self.fc1 = nn.Linear(in_dim, hidden)
+        self.fc2 = nn.Linear(hidden, out_dim)
+        self.relu = nn.ReLU()
+        self.drop = nn.Dropout()
+        self.softmax = nn.Softmax(dim=1)
+        
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = drop(x)
+        x = self.fc2(x)
+        x = self.softmax(x)
+        x = drop(x)
+        return x
