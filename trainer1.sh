caba432e43bececfe9f213b5fb48aeefe73ba4f7
mizunt1 <mizunt@gmail.com>
2020-08-07 12:34:02 +0100

    training with wod 0.001

--- trainer1.sh
+++ trainer1.sh
@@ -4,7 +4,7 @@
 
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
-#SBATCH --job-name="wod5"
+#SBATCH --job-name="wod1"
 #SBATCH --partition="msc"
 
 export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
@@ -16,7 +16,7 @@ source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
 srun python trainer_without_decoder.py\
     --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
     --img_file /scratch-ssd/oatml/data/gz2\
-    --dir_name wod_mike_0.005\
+    --dir_name wod_mike_0.001\
     --arch encoder_decoder_mike.py  --lr 1.0e-4\
-    --num_epochs 1 --img_size 128 --crop_size 128 --z_size 100 --batch_size 10\
-    --subset --subset_proportion 0.005
+    --num_epochs 200 --img_size 128 --crop_size 128 --z_size 100 --batch_size 10\
+    --subset --subset_proportion 0.001
