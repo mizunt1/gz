e17e2eca2d684623cd508bdb64b9e76813d6cfbe
mizunt1 <mizunt@gmail.com>
2020-08-07 12:25:35 +0100

    one layer classifier 0.005 data

--- trainer4.sh
+++ trainer4.sh
@@ -4,7 +4,7 @@
 
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
-#SBATCH --job-name="drop4"
+#SBATCH --job-name="0.005"
 #SBATCH --partition="msc"
 
 export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
@@ -17,7 +17,7 @@ srun python trainer_vae_classifier_ss.py\
      --arch encoder_decoder_pose_vae_split.py\
      --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
      --img_file /scratch-ssd/oatml/data/gz2\
-     --dir_name pose_ss_drop_4layer\
-     --num_epochs 200 --img_size 128 --crop_size 128  --batch_size 100\
-     --lr 1e-4 --s_proportion 0.05
+     --dir_name one_layer_class_0.005\
+     --num_epochs 200 --img_size 128 --crop_size 128  --batch_size 10\
+     --lr 1e-4 --s_proportion 0.005
 
