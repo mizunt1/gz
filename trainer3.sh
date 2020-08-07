51de8ae0b5ddcee26f32ae8ffc1f9c4f6f4f9b4c
mizunt1 <mizunt@gmail.com>
2020-08-07 13:03:25 +0100

    batch 10 drop 0.5 2 layer

--- trainer3.sh
+++ trainer3.sh
@@ -4,7 +4,7 @@
 
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
-#SBATCH --job-name="poseclass"
+#SBATCH --job-name="bnn"
 #SBATCH --partition="msc"
 
 export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
@@ -14,10 +14,10 @@ export CONDA_PKGS_DIRS=/scratch-ssd/$USER/conda_pkgs
 source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
  
 
-srun python trainer_pose_vae_classifier.py\
+srun python trainer_vae_bnn_ss.py\
     --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
     --img_file /scratch-ssd/oatml/data/gz2\
-    --dir_name pose_vae_class_trans_all_data\
+    --dir_name testing_bnn\
     --arch encoder_decoder_pose_vae_split.py  --lr 1.0e-4\
-    --num_epochs 200 --img_size 128 --crop_size 128 --z_size 100 --batch_size 100
+    --num_epochs 200 --img_size 128 --crop_size 128 --z_size 100 --batch_size 100 --subset
 
