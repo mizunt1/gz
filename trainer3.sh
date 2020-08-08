0409cc912b686de6fc7e065737ff7bbd82d9e01e
mizunt1 <mizunt@gmail.com>
2020-08-08 11:43:58 +0100

    subset bnn 0.5

--- trainer3.sh
+++ trainer3.sh
@@ -4,7 +4,7 @@
 
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
-#SBATCH --job-name="bnn5"
+#SBATCH --job-name="bnns5"
 #SBATCH --partition="msc"
 
 export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
@@ -16,7 +16,7 @@ source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
 srun python trainer_vae_bnn_ss.py\
     --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
     --img_file /scratch-ssd/oatml/data/gz2\
-    --dir_name bnn_lr5\
-    --arch encoder_decoder_pose_vae_split.py  --lr 1.0e-5\
-    --num_epochs 200 --img_size 128 --crop_size 128 --z_size 100 --batch_size 100
+    --dir_name bnn_lr4_sub_0.5\
+    --arch encoder_decoder_pose_vae_split.py  --lr 1.0e-4\
+    --num_epochs 200 --img_size 128 --crop_size 128 --z_size 100 --batch_size 100 --s_proportion 0.5
 
