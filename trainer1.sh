43aedf9feccf94417a8010085d1210e9c3f69a22
mizunt1 <mizunt@gmail.com>
2020-08-10 11:00:23 +0100

    mike split experimets

--- trainer1.sh
+++ trainer1.sh
@@ -4,7 +4,7 @@
 
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
-#SBATCH --job-name="wod_nr3s"
+#SBATCH --job-name="wod_mike"
 #SBATCH --partition="msc"
 
 export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
@@ -16,7 +16,7 @@ source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
 srun python trainer_without_decoder.py\
     --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
     --img_file /scratch-ssd/oatml/data/gz2\
-    --dir_name wod_nr3s\
-    --arch encoder_decoder_new_res3_split.py  --lr 1.0e-4\
-    --num_epochs 200 --img_size 128 --crop_size 128 --z_size 100 --batch_size 100\
+    --dir_name wod_mike_ss_fin\
+    --arch encoder_decoder_mike.py  --lr 1.0e-4\
+    --num_epochs 100 --img_size 128 --crop_size 128 --z_size 100 --batch_size 100\
     --subset --subset_proportion 0.5
