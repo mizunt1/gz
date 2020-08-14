3af1d3439b5b83b1acc2fa57743b7e0b4aaa191f
mizunt1 <mizunt@gmail.com>
2020-08-14 17:41:52 +0100

    ss 100

--- trainer3.sh
+++ trainer3.sh
@@ -4,7 +4,7 @@
 
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
-#SBATCH --job-name="bnns.005"
+#SBATCH --job-name="ss100"
 #SBATCH --partition="msc"
 
 export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
@@ -13,10 +13,10 @@ export CONDA_PKGS_DIRS=/scratch-ssd/$USER/conda_pkgs
 /scratch-ssd/oatml/scripts/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f gz_mizu.yml
 source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
  
-srun python trainer_vae_bnn_ss.py\
+srun python trainer_vae_classifier_ss.py\
     --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
     --img_file /scratch-ssd/oatml/data/gz2\
-    --dir_name bnn_lr4_sub_0.005\
+    --dir_name pose_ss_split_100 --class_arch classifier_conv.py\
     --arch encoder_decoder_pose_vae_split.py  --lr 1.0e-4\
-    --num_epochs 200 --img_size 128 --crop_size 128 --z_size 100 --batch_size 10 --s_proportion 0.005
+    --num_epochs 200 --img_size 128 --crop_size 128 --z_size 100 --batch_size 10 --s_proportion 100
 
