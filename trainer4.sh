d9b1db644db02da6639fc35f8b39755f295ee152
mizunt1 <mizunt@gmail.com>
2020-08-22 10:00:39 +0100

    trainer pose vae wod

--- trainer4.sh
+++ trainer4.sh
@@ -4,7 +4,7 @@
 
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
-#SBATCH --job-name="conv_0.05"
+#SBATCH --job-name="pose_wod"
 #SBATCH --partition="msc"
 
 export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
@@ -13,12 +13,11 @@ export CONDA_PKGS_DIRS=/scratch-ssd/$USER/conda_pkgs
 /scratch-ssd/oatml/scripts/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f gz_mizu.yml
 source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
 
-srun python trainer_vae_classifier_ss.py\
+srun python trainer_without_decoder.py\
      --arch encoder_decoder_pose_vae_split.py\
-     --class_arch classifier_conv.py\
      --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
      --img_file /scratch-ssd/oatml/data/gz2\
-     --dir_name classifier_conv_pose_ss_0.05_200\
-     --num_epochs 200 --img_size 128 --crop_size 128  --batch_size 100\
-     --lr 1e-4 --s_proportion 0.05
+     --dir_name wod_pose_conv_100\
+     --num_epochs 200 --img_size 128 --crop_size 128  --batch_size 10\
+     --lr 1e-4 --subset --subset_proportion 100
 
