e19c08d290654b6b4bb1bda6699913eef54c7e3a
mizunt1 <mizunt@gmail.com>
2020-09-12 17:06:09 +0100

    returning core files

--- trainer.sh
+++ trainer.sh
@@ -4,7 +4,7 @@
 
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
-#SBATCH --job-name="mike_1200"
+#SBATCH --job-name="det_100"
 #SBATCH --partition="msc"
 
 export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
@@ -13,10 +13,10 @@ export CONDA_PKGS_DIRS=/scratch-ssd/$USER/conda_pkgs
 /scratch-ssd/oatml/scripts/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f gz_mizu.yml
 source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
 
-srun python train_mike_arch.py\
-    --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
-    --img_file /scratch-ssd/oatml/data/gz2\
-    --dir_name mike_arch_1200\
-    --lr 1.0e-4\
-    --num_epochs 200 --img_size 128 --crop_size 128 --batch_size 10\
-    --subset --subset_proportion 1200.0
+srun python trainer_vae_classifier_ss.py\
+     --arch encoder_decoder_pose_vae_split.py\
+     --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
+     --img_file /scratch-ssd/oatml/data/gz2\
+     --dir_name det_100\
+     --num_epochs 200 --img_size 128 --crop_size 128  --batch_size 10\
+     --lr 1e-4 --subset --subset_proportion 0.5
