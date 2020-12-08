baa00d71c4f3e0c55b4604d5bd1972e0a5e52a1d
mizunt1 <mizunt@gmail.com>
2020-12-08 14:53:52 +0000

    updated bash script for sacred runs

--- oatml_bash.sh
+++ oatml_bash.sh
@@ -12,11 +12,4 @@ export CONDA_PKGS_DIRS=/scratch-ssd/$USER/conda_pkgs
 /scratch-ssd/oatml/scripts/run_locked.sh /scratch-ssd/oatml/miniconda3/bin/conda-env update -f gz_mizu.yml
 source /scratch-ssd/oatml/miniconda3/bin/activate gz_mizu
 
-srun python trainer.py\
-     --arch neural_networks/encoder_decoder_pose_vae_split.py\
-     --class_arch neural_networks/classifier_fc.py\
-     --csv_file /scratch-ssd/oatml/data/gz2/gz2_classifications_and_subjects.csv\
-     --dir_name det_100\
-     --img_file /scratch-ssd/oatml/data/gz2\
-     --num_epochs 200 --img_size 128 --crop_size 128  --batch_size 100\
-     --lr 1e-4 --subset --subset_proportion 0.5 --semi-supervised 
+srun python trainer.py with config.yaml --file_storage=my_runs
