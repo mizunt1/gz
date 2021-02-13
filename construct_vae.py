0c8ebe8fa4f57e8ad2e15fab4f5738e497710539
mizunt1 <mizunt@gmail.com>
2021-02-13 08:18:06 +0000

    changed for laplace

--- construct_vae.py
+++ construct_vae.py
@@ -86,7 +86,7 @@ class PoseVAE(nn.Module):
 
             # view from decoder outputs an image
             pyro.sample(
-                "pixels", D.Bernoulli(transformed_view).to_event(3), obs=data)
+                "pixels", D.Laplace(transformed_view).to_event(3), obs=data)
 
     def guide(self, data):
         """
